b2c3f431e58b84e72a345ad39fe8fcc3e550e44b
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-04 19:15:26 +0100

    Update tests.py

--- mysite/bookstore/tests.py
+++ mysite/bookstore/tests.py
@@ -1,10 +1,20 @@
 from django.test import TestCase, RequestFactory
+import json
 from . import views, models
 
 class Main_test(TestCase):
+
+    models.Book.objects.create(name="lion", author="roar")
+    models.Book.objects.create(name="cat", author="meow")
+
     factory = RequestFactory()
     request = factory.get('/book/library')
     response = views.library(request)
 
     def test_response(self):
         self.assertEqual(self.response.status_code, 200)
+
+    def test_list(self):
+        response_data = json.loads(self.response.content)
+        print("New data:   ", response_data)
+        self.assertIsInstance(response_data, list)
