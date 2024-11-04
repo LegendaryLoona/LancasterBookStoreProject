3ad3d0871449aed4f5af985b5ffcf1b62fbbfe87
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-04 19:06:11 +0100

    Add files via upload

--- mysite/bookstore/tests.py
+++ mysite/bookstore/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from . import views, models
 
-# Create your tests here.
+class Main_test(TestCase):
+    factory = RequestFactory()
+    request = factory.get('/book/library')
+    response = views.library(request)
+
+    def test_response(self):
+        self.assertEqual(self.response.status_code, 200)
