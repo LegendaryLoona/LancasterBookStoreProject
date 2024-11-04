83ad1e3100bcab46af5240baf697ff86fc9555da
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-04 20:02:44 +0100

    Update tests.py

--- mysite/bookstore/tests.py
+++ mysite/bookstore/tests.py
@@ -4,17 +4,38 @@ from . import views, models
 
 class Main_test(TestCase):
 
-    models.Book.objects.create(name="lion", author="roar")
-    models.Book.objects.create(name="cat", author="meow")
+    # models.Book.objects.create(name="lion", author="roar")
+    # models.Book.objects.create(name="cat", author="meow")
 
-    factory = RequestFactory()
-    request = factory.get('/book/library')
-    response = views.library(request)
+    def setUp(self):
+        # Create test data
+        models.Book.objects.create(name="Sample Book 1", author="Jalal")
+        models.Book.objects.create(name="Sample Book 2", author="Jalal")
+        models.Book.objects.create(name="Sample Book 3", author="Another Author")
+
 
     def test_response(self):
-        self.assertEqual(self.response.status_code, 200)
+        factory = RequestFactory()
+        request = factory.get('/book/library')
+        response = views.library(request)
+        self.assertEqual(response.status_code, 200)
 
     def test_list(self):
-        response_data = json.loads(self.response.content)
+        factory = RequestFactory()
+        request = factory.get('/book/library')
+        response = views.library(request)
+        response_data = json.loads(response.content)
         print("New data:   ", response_data)
         self.assertIsInstance(response_data, list)
+    
+    def test_filter(self):
+        factory = RequestFactory()
+        filter_request = factory.get('/book/library/?author=Jalal')
+        filter_response = views.library(filter_request)
+        response_data = json.loads(filter_response.content)
+        print("Jalal books3:   ", response_data)
+        for i in response_data:
+            self.assertEqual(i['author'], "Jalal")
+
+    # models.Book.objects.filter(name="lion").delete()
+    # models.Book.objects.filter(name="cat").delete()
