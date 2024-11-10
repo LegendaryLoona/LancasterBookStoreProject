0282fced878d040666ef38f565608f7a87df396c
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-10 15:32:14 +0100

    Update models.py

--- mysite/bookstore/models.py
+++ mysite/bookstore/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default=None)
+    author = models.CharField(max_length=50, default=None)
+    price = models.FloatField(max_length=6, default=None)
+    edition = models.CharField(max_length=10, default=None)
+    description = models.TextField(default=None)
+
+class Author(models.Model):
+    name = models.CharField(max_length=50, default=None)
+    list_of_books = models.TextField(default=None)
