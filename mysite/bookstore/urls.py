f0504e6d1ea9480d71e3bfe1dda20e8722a82e2a
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-10 18:13:26 +0100

    Update urls.py

--- mysite/bookstore/urls.py
+++ mysite/bookstore/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('library/', views.library, name='sample_json_view'),
+    path('addauthor/', views.addauthor, name='sample_json_view'),
+    path('addbook/', views.addbook, name='sample_json_view'),
 ]
