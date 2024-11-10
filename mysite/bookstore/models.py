4f11c7f93cbb03f2122461c1ff3e9831cc20684b
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-10 15:35:50 +0100

    Update models.py

--- mysite/bookstore/models.py
+++ mysite/bookstore/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Book(models.Model):
+    bid = models.IntegerField(default=None)
     name = models.CharField(max_length=50, default=None)
     author = models.CharField(max_length=50, default=None)
     price = models.FloatField(max_length=6, default=None)
@@ -9,5 +10,6 @@ class Book(models.Model):
     description = models.TextField(default=None)
 
 class Author(models.Model):
+    aid = models.IntegerField(default=None)
     name = models.CharField(max_length=50, default=None)
     list_of_books = models.TextField(default=None)
