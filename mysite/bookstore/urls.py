def5597c03d1842dd674400138ec06eb0b864e49
Jalal <79722638+Jalal1302@users.noreply.github.com>
2024-11-14 15:05:33 +0100

    Update urls.py
    added path for editbook

--- mysite/bookstore/urls.py
+++ mysite/bookstore/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('addbook/', views.addbook, name='sample_json_view'),
     path('deletebook/', views.delete_book, name='sample_json_view'),
     path('editauthor/', views.edit_author, name='sample_json_view'),
+    path('editbook/', views.edit_book, name='edit_book'),
     path('showauthors/', views.show_authors, name='sample_json_view'),
     path('deleteauthor/', views.delete_author, name='sample_json_view'),
 ]
