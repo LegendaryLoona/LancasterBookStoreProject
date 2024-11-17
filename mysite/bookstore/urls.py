59b024162fa53399fa5b828a9aedb55630bfd3b8
LegendaryLoona <139046578+LegendaryLoona@users.noreply.github.com>
2024-11-17 16:22:25 +0100

    Update urls.py

--- mysite/bookstore/urls.py
+++ mysite/bookstore/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('addbook/', views.addbook, name='sample_json_view'),
     path('deletebook/', views.delete_book, name='sample_json_view'),
     path('editauthor/', views.edit_author, name='sample_json_view'),
-    path('editbook/', views.edit_book, name='edit_book'),
     path('showauthors/', views.show_authors, name='sample_json_view'),
     path('deleteauthor/', views.delete_author, name='sample_json_view'),
+    path('editbook/', views.edit_book, name='sample_json_view'),
+    path('sortalph/', views.sort_alph, name='sample_json_view'),
+    path('sortprice/', views.sort_price, name='sample_json_view'),
+    path('search/', views.search_books, name='sample_json_view'),
 ]
