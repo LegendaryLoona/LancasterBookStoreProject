327aeae6c750e79fe6eeb9b58b8b88d0abcff529
maomao7788 <maomaorc@foxmail.com>
2024-11-20 05:50:08 +0800

    Update urls.py

--- mysite/bookstore/urls.py
+++ mysite/bookstore/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('library/', views.library, name='sample_json_view'),
     path('addauthor/', views.addauthor, name='sample_json_view'),
     path('addbook/', views.addbook, name='sample_json_view'),
     path('delete_book/', views.delete_book, name='delete_book'),
